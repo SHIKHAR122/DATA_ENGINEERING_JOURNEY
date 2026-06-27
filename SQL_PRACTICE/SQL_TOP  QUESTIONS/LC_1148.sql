588b864c540888b56b617ca6dbbab6dddb07271a
SHIKHAR122 <shikharsharmafeb@gmail.com>
2026-06-27 16:19:31 +0530

    Pushed Today's SQL problem

---
+++ SQL_PRACTICE/SQL_TOP  QUESTIONS/LC_1148.sql
@@ -0,0 +1,13 @@
+--  LEETCODE PROBLEM NUMBER 1148 - ARTICLE VIEWS 
+-- Write a solution to find all the authors that viewed at least one of their own articles.
+
+-- Return the result table sorted by id in ascending order.
+
+
+
+SELECT 
+    author_id as id
+FROM Views
+WHERE author_id = viewer_id 
+GROUP BY author_id 
+ORDER BY id;
