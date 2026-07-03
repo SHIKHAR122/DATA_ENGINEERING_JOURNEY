7ba2b914dfb4e027ac037a06aebc4fc3455c103f
SHIKHAR122 <shikharsharmafeb@gmail.com>
2026-07-03 22:21:49 +0530

    Pushed Today's SQL problems

--- SQL_PRACTICE/HACKERRANK_PROBLEMS/Q2.sql
+++ SQL_PRACTICE/HACKERRANK_PROBLEMS/Q2.sql
@@ -1,7 +1,7 @@
 --  QUESTIONS RELATED TO BASIC AGGREGATES IN HACKERRANK SQL SECTION
 
 
-
+-- QUESTION NUMBER 1
 --Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's  key was broken until after completing the calculation. 
 --She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
 
@@ -14,3 +14,12 @@ FROM Employees;
 
 
 
+
+
+
+-- QUESTION NUMBER 2
+-- Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.770 . Round your answer to 4 decimal places.
+
+SELECT ROUND(LONG_W , 4) FROM STATION WHERE LAT_N = ( SELECT MIN(LAT_N) FROM STATION 
+WHERE LAT_N>38.7780
+)
