a451d3dd9fc2724e30ba1e2d2385e65e9ea81e1e
SHIKHAR122 <shikharsharmafeb@gmail.com>
2026-09-12 16:05:51 +0530

    Pushed Today's Leetcode Problem

--- LEETCODE_PRACTICE/RECURSION/LC_50.cpp
+++ LEETCODE_PRACTICE/RECURSION/LC_50.cpp
@@ -18,3 +18,18 @@ public:
 
 //APPROACH NUMBER - 2 THE RECURSION + MATHS APPROACH 
 
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==1) return x;   //base case for n=1
+        if(n==0) return 1;  //base case for n=0 
+        if(n<0) return 1/myPow(x,-(long long)n); //base case for n<0(negative)
+        double half = myPow(x, n / 2);   //recursive case
+        if(n%2==0)
+        {
+            return half*half;
+        }
+
+        return half*half*x;
+    }
+};
